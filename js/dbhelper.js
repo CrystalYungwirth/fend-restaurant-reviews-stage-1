f33c8b63bedb9de56a1541a514e9dc795303a84c
Crystal Yungwirth <crystalyungwirth@gmail.com>
2018-08-24 19:53:21 -0400

    feat: service worker

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -6,7 +6,7 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker
       .register('sw.js')
       .then(reg => console.log(`Service worker registered. ${reg.scope}`))
-      .catch(err => console.log(`Service worker not registered. Error: ${err}`));
+      .catch(err => console.log(`Service worker not registered. ${err}`));
   });
 }
 
--- sw.js
+++ sw.js
@@ -1,1 +1,39 @@
+// Capturing current date to use to create caches version name. 
+// Using .toLocaleDateString to shorten version name.
+const date = new Date().toLocaleDateString();
+const cacheName = `v.${date}`;
 
+/**
+ *@description cache entire response
+ */
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+    .then(response => {
+      const responseClone = response.clone();
+      caches
+        .open(cacheName)
+        .then(cache => {
+          cache.put(e.request, responseClone);
+        });
+      return response;
+    }).catch(error => caches.match(e.request).then(response => response))
+  );
+});
+
+/**
+ *@description deletes prior cache
+ */
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== cacheName) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
